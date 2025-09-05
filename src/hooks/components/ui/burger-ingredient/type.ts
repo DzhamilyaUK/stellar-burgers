5cd38e45f4bf5e8fe2eef9483e659781c2ca4a1d
Dzhamilya Ukarkhanova <d.t.ukarkhanova@yandex.ru>
2025-09-05 12:30:58 +0300

    юнит тесты

--- src/hooks/components/ui/burger-ingredient/type.ts
+++
@@ -1,9 +0,0 @@
-import { Location } from 'react-router-dom';
-import { TIngredient } from '@utils-types';
-
-export type TBurgerIngredientUIProps = {
-  ingredient: TIngredient;
-  count: number;
-  handleAdd: () => void;
-  locationState?: { background: Location };
-};
--- src/hooks/components/ui/orders-list/type.ts
+++
@@ -1,6 +0,0 @@
-import { TOrder } from '@utils-types';
-
-export type OrdersListUIProps = {
-  orderByDate: TOrder[];
-  basePath?: string;
-};
